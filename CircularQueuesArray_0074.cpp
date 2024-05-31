2375899ef8805b286c796088dd05ac71647b42e5
HaniefuL <haniefulgeofarry@gmail.com>
2024-05-31 14:55:57 +0700

    Menambahkan input nomor yang akan dimasukkan

--- CircularQueuesArray_0074.cpp
+++ CircularQueuesArray_0074.cpp
@@ -10,4 +10,15 @@ public:
         FRONT = -1;
         REAR = -1;
     }
+
+    void insert() {
+        int num;
+        cout << "Enter a number : ";
+        cin >> num;
+        cout << endl;
+
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+            cout << "\nQueue overflow\n";
+            return;
+        }
 };
