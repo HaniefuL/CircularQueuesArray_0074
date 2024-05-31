e2d37c13460a062f3bcaa6dffc25f299a3657a63
HaniefuL <haniefulgeofarry@gmail.com>
2024-05-31 15:35:30 +0700

    menambahkan case 3

--- CircularQueuesArray_0074.cpp
+++ CircularQueuesArray_0074.cpp
@@ -112,6 +112,10 @@ int main(){
                 q.remove();
                 break;
             }
+            case '3': {
+                q.display();
+                break;
+            }
         }
     }
 }
